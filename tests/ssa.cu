1d68207853df2820e8838ca1c28bb0e720056f1a
voduchuy <vdhuy91@gmail.com>
2020-05-10 14:00:19 -0600

    Fix bug introduced by using thrust::inclusive_scan within kernel.

--- tests/ssa.cu
+++ tests/ssa.cu
@@ -39,7 +39,7 @@ void initialize_ribosome_locations(const int num_rib, int *X) {
 }
 
 __device__
-int draw_uniforms(curandState_t *rstate, double *rn) {
+void draw_uniforms(curandState_t *rstate, double *rn) {
   rn[0] = curand_uniform_double(rstate);
   rn[1] = curand_uniform_double(rstate);
 }
@@ -106,7 +106,7 @@ void update_state(const int num_times,
   int &to_shift = *to_shift_ptr;
   const double &t_final = t_array[num_times - 1];
 
-  // INITIALIZARION
+  // INITIALIZATION
   // Init time
   if (thread_id == 0) {
     t_now = 0.0;
@@ -117,14 +117,13 @@ void update_state(const int num_times,
   // Copy initial ribosome locations to shared memory
   uint idx;
   uint n_passes = num_rib / blockDim.x;
-
   for (uint k{0}; k < n_passes; ++k) {
     x_shared[k * blockDim.x + thread_id] = X[sample_id * num_rib + k * blockDim.x + thread_id];
   }
   if (thread_id < num_rib % blockDim.x) {
     x_shared[n_passes * blockDim.x + thread_id] = X[sample_id * num_rib + n_passes * blockDim.x + thread_id];
   }
-
+  __syncthreads();
 
   // STEPPING
   while (t_now < t_final) {
@@ -138,8 +137,10 @@ void update_state(const int num_times,
       idx = num_rib - num_rib % blockDim.x + thread_id;
       x_wsp[idx] = probe_design[x_shared[idx]];
     }
-    thrust::inclusive_scan(thrust::device, x_wsp, x_wsp + num_rib, x_wsp);
-    current_intensity = x_wsp + num_rib - 1;
+    __syncthreads();
+    if (thread_id == 0) thrust::inclusive_scan(thrust::seq, x_wsp, x_wsp + num_rib, x_wsp);
+    __syncthreads();
+    if (thread_id == 0) current_intensity = x_wsp + num_rib - 1;
     // copy current intensity to appropriate locations in global memory
     if (thread_id == 0) {
       while (t_array[*idx_to_output] <= t_now) {
@@ -157,60 +158,70 @@ void update_state(const int num_times,
     for (uint k{0}; k < n_passes; ++k) {
       idx = k * blockDim.x + thread_id;
       propensities[idx] = rates[x_shared[idx]] * (
-          (idx == 0)
-              +
-                  (idx != 0)
-                      *
-                          (x_shared[idx - 1] - x_shared[idx] > num_excl)
+          (idx == 0) + (idx != 0) * (x_shared[idx - 1] - x_shared[idx] > num_excl)
       );
     }
     if (thread_id < num_rib % blockDim.x) {
-      idx = num_rib - num_rib % blockDim.x + thread_id;
-      propensities[idx] = rates[x_shared[idx]] *
-          (
-              (idx == 0)
-                  +
-                      (idx != 0)
-                          *
-                              (x_shared[idx - 1] - x_shared[idx] > num_excl)
-          );
-
+      idx = n_passes * blockDim.x + thread_id;
+      propensities[idx] = rates[x_shared[idx]] * (
+          (idx == 0) + (idx != 0) * (x_shared[idx - 1] - x_shared[idx] > num_excl)
+      );
     }
     __syncthreads();
 
     // transform the propensities array to its cumsum array
-    thrust::inclusive_scan(thrust::device, propensities, propensities + num_rib, propensities);
+    if (thread_id == 0) {
+//      for (int i{1}; i < num_rib; ++i){
+//        propensities[i] += propensities[i-1];
+//      }
+      thrust::inclusive_scan(thrust::device, propensities, propensities + num_rib, propensities);
+    }
+    __syncthreads();
 
-    // determine stepsize and the next ribosome to move
+    // determine stepsize
     if (thread_id == 0) {
       draw_uniforms(rstates + sample_id, rn);
       stepsize = -1.0 * log(rn[0]) / propensities[num_rib - 1];
     }
-    t_now += stepsize;
-    // update state
+    __syncthreads();
+
+    // choose the ribosome to move
+    n_passes = num_rib / blockDim.x;
+    for (uint k{0}; k < n_passes; ++k) {
+      idx = k * blockDim.x + thread_id;
+      x_wsp[idx] = (propensities[idx] >= rn[1] * propensities[num_rib - 1]);
+    }
+    if (thread_id < num_rib % blockDim.x) {
+      idx = n_passes * blockDim.x + thread_id;
+      x_wsp[idx] = (propensities[idx] >= rn[1] * propensities[num_rib - 1]);
+    }
+    __syncthreads();
+    if (thread_id == 0) idx = thrust::find(thrust::device, x_wsp, x_wsp + num_rib, 1) - x_wsp;
+    __syncthreads();
     if (thread_id == 0) {
       if (t_now + stepsize <= t_final) {
-        for (int i{0}; i < num_rib; ++i) {
-          if (propensities[i] >= rn[1] * propensities[num_rib - 1]) {
-            x_shared[i] = (x_shared[i] + 1) % (gene_len + 1);
-            if (x_shared[i] == 0) {
-              to_shift = i + 1;
-            }
-            break;
-          }
+        t_now += stepsize;
+        x_shared[idx] = (x_shared[idx] + 1) % (gene_len + 1);
+        if (x_shared[idx] == 0) {
+          to_shift = ( int ) idx + 1;
         }
+      } else {
+        t_now = t_final;
       }
     }
     __syncthreads();
     // check if we need to shift ribosomes locations so that the first nonzero is at the beginning
     if (to_shift > 0) {
       shift_arrays(to_shift, num_rib, x_shared, x_wsp);
+
+      __syncthreads();
       if (thread_id == 0) {
         to_shift = 0;
       }
     }
+    __syncthreads();
   }
-  // Compute intensity at final time
+  // Compute current intensity
   n_passes = num_rib / blockDim.x;
   for (uint k{0}; k < n_passes; ++k) {
     idx = k * blockDim.x + thread_id;
@@ -220,23 +231,27 @@ void update_state(const int num_times,
     idx = num_rib - num_rib % blockDim.x + thread_id;
     x_wsp[idx] = probe_design[x_shared[idx]];
   }
-  thrust::inclusive_scan(thrust::device, x_wsp, x_wsp + num_rib, x_wsp);
-  current_intensity = x_wsp + num_rib - 1;
+  __syncthreads();
+  if (thread_id == 0) thrust::inclusive_scan(thrust::device, x_wsp, x_wsp + num_rib, x_wsp);
+  __syncthreads();
+  if (thread_id == 0) current_intensity = x_wsp + num_rib - 1;
   // copy current intensity to appropriate locations in global memory
-  if (thread_id == 0) {
-    intensity[sample_id * num_times + num_times - 1] = (*current_intensity);
+  if (thread_id == 0 && (*idx_to_output < num_times)) {
+    while (t_array[*idx_to_output] <= t_now) {
+      intensity[sample_id * num_times + (*idx_to_output)] = (*current_intensity);
+      (*idx_to_output)++;
+      if (*idx_to_output >= num_times) {
+        break;
+      }
+    }
   }
+  __syncthreads();
 
 // COPY FINAL RIBOSOMES LOCATIONS TO GLOBAL MEMORY
   n_passes = num_rib / blockDim.x;
-  for (
-      int k{0};
-      k < n_passes;
-      ++k) {
+  for (int k{0}; k < n_passes; ++k) {
     idx = thread_id + k * blockDim.x;
-    X[
-        sample_id * num_rib
-            + idx] = x_shared[idx];
+    X[sample_id * num_rib + idx] = x_shared[idx];
   }
   if (thread_id < num_rib % blockDim.x) {
     X[
@@ -250,13 +265,13 @@ void update_state(const int num_times,
 }
 
 int main(int argc, char **argv) {
-  const int num_rib_max = 64;
-  const int num_samples = 1;
+  const int num_rib_max = 256;
+  const int num_samples = 100;
   const int n_excl = 3;
-  const int gene_len = 10;
+  const int gene_len = 100;
 
-  const int num_times = 5;
-  const double t_array[] = {0.0, 10.0, 20.0, 50.0, 1000.0};
+  const int num_times = 4;
+  const double t_array[] = {0.0, 10.0, 100.0, 1000.0};
 
   thrust::device_vector<double> rates(gene_len + 1, 1.0);
   thrust::device_vector<int> probe_design(gene_len + 1);
@@ -265,8 +280,8 @@ int main(int argc, char **argv) {
   thrust::fill(probe_design.begin() + 1, probe_design.end(), 1);
   thrust::fill(intensity.begin(), intensity.end(), 0);
 
-  thrust::device_vector<double> t_array_device(5);
-  thrust::copy(&t_array[0], &t_array[5], t_array_device.begin());
+  thrust::device_vector<double> t_array_device(num_times);
+  thrust::copy(&t_array[0], &t_array[num_times], t_array_device.begin());
 
   curandState_t *rand_states;
   cudaMalloc(( void ** ) &rand_states, num_samples * sizeof(curandState_t));
@@ -286,20 +301,22 @@ int main(int argc, char **argv) {
       + num_rib_max * sizeof(int) // for ribosome locations
       + num_rib_max * sizeof(int) // temporary space to copy ribosome locations (when shifting)
       + sizeof(int) // amount to shift
-      + sizeof(int) // current intensity
       + sizeof(int) // time array index to output the intesnity to
   ;
-  update_state<<<num_samples, 64, shared_mem_size>>>(num_times,
-                                                      thrust::raw_pointer_cast(&t_array_device[0]),
-                                                      n_excl,
-                                                      gene_len,
-                                                      num_rib_max,
-                                                      X,
-                                                      rand_states,
-                                                      thrust::raw_pointer_cast(&rates[0]),
-                                                      thrust::raw_pointer_cast(&probe_design[0]),
-                                                      thrust::raw_pointer_cast(&intensity[0]));
+  std::cout << "Simulating...\n";
+  update_state<<<num_samples, 32, shared_mem_size>>>(num_times,
+                                                     thrust::raw_pointer_cast(&t_array_device[0]),
+                                                     n_excl,
+                                                     gene_len,
+                                                     num_rib_max,
+                                                     X,
+                                                     rand_states,
+                                                     thrust::raw_pointer_cast(&rates[0]),
+                                                     thrust::raw_pointer_cast(&probe_design[0]),
+                                                     thrust::raw_pointer_cast(&intensity[0]));
   CUDACHKERR();
+  cudaDeviceSynchronize();
+  std::cout << "Returned from simulation kernel.\n";
 
   int X_host[num_samples][num_rib_max];
   cudaMemcpy(( void * ) X_host, ( void * ) X, num_samples * num_rib_max * sizeof(int), cudaMemcpyDeviceToHost);
@@ -309,10 +326,10 @@ int main(int argc, char **argv) {
   thrust::copy(intensity.begin(), intensity.end(), intensity_host.begin());
 
   for (int i{0}; i < num_samples; ++i) {
-    for (int j{0}; j < num_rib_max; ++j) {
-      std::cout << X_host[i][j] << " ";
-    }
-    std::cout << "\n";
+//    for (int j{0}; j < num_rib_max; ++j) {
+//      std::cout << X_host[i][j] << " ";
+//    }
+//    std::cout << "\n";
     for (int j{0}; j < num_times; ++j) {
       std::cout << intensity_host[i * num_times + j] << " ";
     }
